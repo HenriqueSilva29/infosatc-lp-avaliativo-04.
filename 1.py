05b096bf3b048eb7b9729b13702654af128c6caa
Henrique <69260698+HenriqueSilva29@users.noreply.github.com>
2020-09-29 22:30:13 -0300

    feat: atividade 1

---
+++ 1.py
@@ -0,0 +1,4 @@
+palavra= str(input('Qualquer palavra que vier na sua  mente: '))
+print(' palavra: {}'.format(palavra))
+string = palavra[::-1]
+print('A frase que você digitou invertida fica: {}'.format(string))
